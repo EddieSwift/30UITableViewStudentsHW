35b527d02b7e1403fc4166b23ae189aacb99f0ec
EddieSwift <37484195+EddieSwift@users.noreply.github.com>
2019-01-29 17:34:00 +0200

    Initial Commit

---
+++ 30UITableViewStudentsHW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  30UITableViewStudentsHW
+//
+//  Created by Eduard Galchenko on 1/29/19.
+//  Copyright © 2019 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
